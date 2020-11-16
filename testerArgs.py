fbfca06303a42f8b173a126ef8b76d4e22ea19af
delispeter19 <delispeter19@gmail.com>
2020-11-15 19:33:36 -0500

    hopefully fixed encoding issue!

---
+++ testerArgs.py
@@ -0,0 +1,118 @@
+import sys
+import json
+import csv
+
+jsonFilePath = sys.argv[1]
+
+for j in range(1,21):
+    f = open(jsonFilePath + "\\j" + str(j) + ".json", "r")
+    jsonify = json.loads(f.read())
+    f.close()
+
+    full_description = jsonify[0]["textAnnotations"][0]["description"]
+    split_description = full_description.split("\n")
+
+    found_in = -1
+
+
+    def removeTags(token):
+        simple_token = token.replace(
+            "-", "").replace("(", "").replace(")", "").replace(" ", "").replace(".", "")
+        return simple_token
+
+
+    with open(".\\supplierlist\\supplierlist1.csv",
+              "r", encoding="utf8") as f:
+        supplierList = csv.DictReader(f)
+        matchRow = []
+        i = 0
+        found = False
+        for row in supplierList:
+            if not found:
+                i += 1
+                if row["Phone"] != '0':
+                    number_of_tokens = 15
+                    for token in split_description:
+                        number_of_tokens -= 1
+                        if row["Phone"] in removeTags(token):
+                            print("Found NUM Match!!!")
+                            matchRow = row
+                            found = True
+                            found_in = 1
+                            break
+                        if number_of_tokens == 0:
+                            break
+        f.close()
+
+    if matchRow == []:
+        with open("C:\\Users\\delis\\Documents\\UNI4\\ENGLYMPICS\\CompetitionPackage\\supplierlist\\supplierlist2.csv",
+                  "r", encoding="utf8") as f:
+            supplierList = csv.DictReader(f)
+            matchRow = []
+            i = 0
+            found = False
+            for row in supplierList:
+                if not found:
+                    i += 1
+                    if row["Phone"] != '0':
+                        number_of_tokens = 15
+                        for token in split_description:
+                            number_of_tokens -= 1
+                            if row["Phone"] in removeTags(token):
+                                print("Found NUM Match!!!")
+                                matchRow = row
+                                found = True
+                                found_in = 2
+                                break
+                            if number_of_tokens == 0:
+                                break
+            f.close()
+    if matchRow == []:
+        with open("C:\\Users\\delis\\Documents\\UNI4\\ENGLYMPICS\\CompetitionPackage\\supplierlist\\supplierlist1.csv",
+                  "r", encoding="utf8") as f:
+            supplierList = csv.DictReader(f)
+            matchRow = []
+            i = 0
+            found = False
+            for row in supplierList:
+                if not found:
+                    i += 1
+                    number_of_tokens = 15
+                    for token in split_description:
+                        number_of_tokens -= 1
+                        if len(row["Business Name"]) > 3 and row["Business Name"].lower() in token.lower():
+                            print("Found WORD Match!!!")
+                            matchRow = row
+                            found = True
+                            found_in = 1
+                            break
+                        if number_of_tokens == 0:
+                            break
+            f.close()
+
+    if not matchRow:
+        with open("C:\\Users\\delis\\Documents\\UNI4\\ENGLYMPICS\\CompetitionPackage\\supplierlist\\supplierlist2.csv",
+                  "r", encoding="utf8") as f:
+            supplierList = csv.DictReader(f)
+            matchRow = []
+            i = 0
+            found = False
+            for row in supplierList:
+                if not found:
+                    i += 1
+                    number_of_tokens = 15
+                    for token in split_description:
+                        number_of_tokens -= 1
+                        if len(row["Business Name"]) > 3 and row["Business Name"].lower() in token.lower():
+                            print("Found WORD Match!!!")
+                            matchRow = row
+                            found = True
+                            found_in = 2
+                            break
+                        if number_of_tokens == 0:
+                            break
+            f.close()
+    print(("Done Running File " + str(j) + ". \n\t Found at row: " + str(i + 1) + " in supplierlist" +
+          str(found_in) + ".csv" + "\n\t Supplier Name: " + matchRow["Business Name"] + "\n\t SIC4: " + matchRow["SIC4 Category"] + "\n\t SIC8: " + matchRow["SIC8 Category"]).replace('\ufffd', ''))
+    # print("Done Running File " + str(j) + ". \n\t Found at row: " + str(i + 1) + " in supplierlist" +
+    #       str(found_in) + ".csv" + "\n\t Supplier Name: " + matchRow["Business Name"] + "\n\t SIC4: " + matchRow["SIC4 Category"] + "\n\t SIC8: " + matchRow["SIC8 Category"].replace())
